a9b6c46c5de35bbe00824e9a520d982eea2cc7e5
DedeHai <daedae@gmx.ch>
2016-03-05 08:18:02 +0100

    initial commit

---
+++ Arduino/FablabUhr/clock.ino
@@ -0,0 +1,115 @@
+//draw hands of clock
+
+#define HOURTAIL 3
+#define MINUTETAIL 5
+#define SECONDTAIL 10
+
+void drawFace(RGB tickcolor)
+{
+  uint8_t i;
+  for(i=0; i<12;i++)
+  {
+    
+       pixelmatrix.setColor(60 - ((i*5 + 30) % 61),  0, tickcolor);
+  
+    }
+  
+  }
+
+void drawHands(RGB hourcolor, RGB mincolor, RGB seccolor, uint8_t tail)
+{
+
+
+  uint8_t hourpixel = 60 - (((hour() % 12) * 5 + (int)minute() * 5 / 60 + 30) % 61);
+  uint8_t minutepixel = 60 - ((minute() + 30) % 61);
+  uint8_t secondpixel = 60 - ((second() + 30) % 61);
+
+  addcolor(minutepixel,  0, mincolor);
+  addcolor(secondpixel,  0, seccolor);
+  addcolor(hourpixel,  0, hourcolor);
+
+  if (tail)
+  {
+    RGB black;
+    black.r = 0;
+    black.g = 0;
+    black.b = 0;
+
+    uint8_t i;
+
+    for (i = 1; i <= HOURTAIL; i++)
+    {
+      hourpixel++;
+      hourpixel = hourpixel % 60;
+      addcolor(hourpixel,  0,  fadecolorRGB(i, HOURTAIL, hourcolor,black));
+     
+    }
+    
+    for (i = 1; i <= MINUTETAIL; i++)
+    {
+      minutepixel++;
+      minutepixel = minutepixel % 60;
+      addcolor(minutepixel,  0,  fadecolorRGB(i, MINUTETAIL, mincolor,black));
+    }
+    
+    
+    for (i = 1; i <= SECONDTAIL; i++)
+    {
+      secondpixel++;
+      secondpixel = secondpixel % 60;
+      addcolor(secondpixel,  0,  fadecolorRGB(i, SECONDTAIL, seccolor,black));
+    }
+    }
+
+
+}
+
+
+
+void drawHands(uint8_t hourhue, uint8_t minhue, uint8_t sechue, uint8_t tail)
+{
+
+
+  uint8_t hourpixel = 60 - (((hour() % 12) * 5 + (int)minute() * 5 / 60 + 30) % 60);
+  uint8_t minutepixel = 60 - ((minute() + 30) % 60);
+  uint8_t secondpixel = 60 - ((second() + 30) % 60);
+
+  addcolor(minutepixel,  0,  hsv_to_rgb (minhue, 255, 255));
+  addcolor(secondpixel,  0, hsv_to_rgb (sechue, 255, 255));
+  addcolor(hourpixel,  0, hsv_to_rgb (hourhue, 255, 255));
+
+  if (tail)
+  {
+    RGB black;
+    black.r = 0;
+    black.g = 0;
+    black.b = 0;
+
+    uint8_t i;
+
+    for (i = 1; i <= HOURTAIL; i++)
+    {
+      hourpixel++;
+      hourpixel = hourpixel % 60;
+      addcolor(hourpixel,  0,  hsv_to_rgb (hourhue, 255,((int)255*(HOURTAIL-i)/HOURTAIL)));
+     
+    }
+    
+    for (i = 1; i <= MINUTETAIL; i++)
+    {
+      minutepixel++;
+      minutepixel = minutepixel % 60;
+      addcolor(minutepixel,  0,  hsv_to_rgb (minhue, 255,((int)255*(MINUTETAIL-i)/MINUTETAIL)));
+    }
+    
+    
+    for (i = 1; i <= SECONDTAIL; i++)
+    {
+      secondpixel++;
+      secondpixel = secondpixel % 60;
+      addcolor(secondpixel,  0,    hsv_to_rgb (sechue, 255,((int)255*(SECONDTAIL-i)/SECONDTAIL)));
+    }
+    }
+
+
+}
---
+++ Arduino/FablabUhr/helpers.ino
@@ -0,0 +1,82 @@
+/*
+mögliche uhren:
+-stunde:2 oder drei breit, minute und sekunde 1 breit. Hintergrundfarbe könnte ändern, einmal im farbkreis pro stunde, zeiger genau invertieren, bei überlappung schwarz oder sättigung verringern oder hue addieren
+-mit einer zufallsfarbe füllen oder mit farbschemas füllen zwischen den Zeigern. bei überlappung farben addieren
+
+animationen:
+farben abwechseln, sektoren teilen und überschreiben oder sektoren rotieren
+überblenden auch möglich, müsste man farbbuffer wieder einführen
+Animationen kann man mit uhr überlagern, dann zeiger einfach schwarz oder weiss färben
+
+
+*/
+
+void rotatesectors(uint8_t color1width, uint8_t segments, RGB color1, RGB color2)
+{
+  static uint8_t rotation = 0;
+  uint8_t i;
+
+  //check if segmentation is possible, set to default if not
+  if ((color1width + 1)* segments > 60)
+  {
+    segments = 6;
+    color1width = 5;
+  }
+
+  uint8_t color2width = (60 / segments) - color1width;
+  uint8_t maxrotation = color1width + color2width;
+
+  for (i = 0; i < 60; i++)
+  {
+
+    pixelmatrix.setColor(i + rotation,  0, color1);
+
+
+
+  }
+
+
+}
+
+
+
+void addcolor(uint8_t x, uint8_t y, RGB color)
+{
+  RGB result = pixelmatrix.getColor(x, y);
+  uint16_t redsum = (uint16_t)result.r + (uint16_t)color.r;
+  uint16_t greensum = (uint16_t)result.g + (uint16_t)color.g;
+  uint16_t bluesum = (uint16_t)result.b + (uint16_t)color.b;
+
+  if (redsum > 255) redsum = 255;
+  if (greensum > 255) greensum = 255;
+  if (bluesum > 255) bluesum = 255;
+
+  result.r = redsum;
+  result.g = greensum;
+  result.b = bluesum;
+
+  pixelmatrix.setColor(x,  y, result);
+
+}
+
+
+
+RGB fadecolorRGB(uint8_t fadestep, uint8_t totalfadesteps, RGB startcolor,
+                 RGB endcolor) {
+  RGB returncolor;
+  float factor = (float) (fadestep) / (float) totalfadesteps;
+  returncolor.r = (uint8_t)(
+                    startcolor.r
+                    - (factor)
+                    * ((int16_t) startcolor.r - (int16_t) endcolor.r));
+  returncolor.g = (uint8_t)(
+                    startcolor.g
+                    - (factor)
+                    * ((int16_t) startcolor.g - (int16_t) endcolor.g));
+  returncolor.b = (uint8_t)(
+                    startcolor.b
+                    - (factor)
+                    * ((int16_t) startcolor.b - (int16_t) endcolor.b));
+  return returncolor;
+}
+
